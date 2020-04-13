2db5bfd2455e73da65a3bed99421d81df4c94c77
TryNeo <56527327+TryNeo@users.noreply.github.com>
2020-04-13 11:30:49 -0500

    Login Database

--- database/login_db.sql
+++ database/login_db.sql
@@ -1,7 +1,7 @@
 drop database if exists login;
 create database if not exists login;
 use login;
-create table usuario(
+create table usuarios(
 id_usuario int auto_increment,
 email varchar(50),
 username varchar(50),
